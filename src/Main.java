160c9431240533af8b26b311410a62d34d0d6e22
imranrasulzade <imranrasulzade5@gmail.com>
2026-06-13 19:27:44 +0400

    some

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        Scanner input = new Scanner(System.in);
-
-        int grade = input.nextInt();
-
-        if (grade >= 91) {
-            
+        int a = 3;
+        int b = 2;
+        int c = 9;
 
 
+        int cem = a + b + c;
+        double average =  (double) cem / 3;
 
-        } else {
 
-        }
 
 
 
